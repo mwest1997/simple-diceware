4a3b9e07fa2da900619b914faf31eea2fbe5f936
Austin <mwest29@cnm.edu>
2020-10-15 16:58:40 -0600

    Second commit.

--- src/main/java/edu/cnm/deepdive/simplediceware/configuration/Beans.java
+++ src/main/java/edu/cnm/deepdive/simplediceware/configuration/Beans.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.simplediceware.configuration;
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.ResourceBundle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Beans {
 
+  @Value("${diceware.bundle}")
   private String wordListBundle;
 
   @Bean
@@ -20,5 +22,5 @@ public class Beans {
   public ResourceBundle bundle() {
     return ResourceBundle.getBundle(wordListBundle);
   }
-  
+
 }
