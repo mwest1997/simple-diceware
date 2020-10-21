d9b8be583d616fe3d527ea1bb6a27cc95cd851da
Austin <mwest29@cnm.edu>
2020-10-21 16:19:47 -0600

    Implemented persistence of passphrases.

--- src/main/java/edu/cnm/deepdive/simplediceware/controller/DicewareController.java
+++ src/main/java/edu/cnm/deepdive/simplediceware/controller/DicewareController.java
@@ -4,10 +4,12 @@ import edu.cnm.deepdive.simplediceware.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/simple")
 public class DicewareController {
 
   private static final String LENGTH_PARAMETER = "length";
